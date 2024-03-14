c2c12edcaf95daa7cecee15dfc8efbcb4b0503a9
tehmeerali786 <freelanceali786@gmail.com>
2024-03-13 19:38:50 -0700

    Chapter 3 Search 2

--- Chapter 3/mysite/blog/views.py
+++ Chapter 3/mysite/blog/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView 
 from django.core.mail import send_mail 
 from django.views.decorators.http import require_POST
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 
@@ -110,6 +110,8 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)).filter(rank__gte=0.3).order_by('-rank')
             
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
